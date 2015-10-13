f0e7137aaf29f981c6b5ccd07a915be498521157
Kyle Petrovich <kylepetrovich@gmail.com>
2015-10-13 17:58:46 -0400

    minification

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
-module.exports = {
+var webpack = require('webpack');
+
+module.exports = [
+  {
     entry: './src/jsx/main.jsx',
     output: {
         path: __dirname + '/dist',
@@ -16,4 +19,27 @@ module.exports = {
     resolve: {
         extensions: ['', '.js', '.json', '.jsx']
     }
-}
+  },
+  {
+    entry: './src/jsx/main.jsx',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.min.js'
+    },
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin()
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.json', '.jsx']
+    }
+  }
+];
